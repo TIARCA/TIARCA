0ce6539b1e36326b36def4ab9d12a1a2303d5eb3
TIARCA <marcovange@duck.com>
2026-09-11 11:11:58 +0200

    Carry automated sender metadata with messages

--- chatlib/src/main/java/io/mrarm/chatlib/dto/MessageSenderInfo.java
+++ chatlib/src/main/java/io/mrarm/chatlib/dto/MessageSenderInfo.java
@@ -7,12 +7,20 @@ public class MessageSenderInfo extends NickWithPrefix {
     private String user;
     private String host;
     private UUID userUUID;
+    private boolean automated;
 
-    public MessageSenderInfo(String nick, String user, String host, NickPrefixList nickPrefixes, UUID userUUID) {
+    public MessageSenderInfo(String nick, String user, String host, NickPrefixList nickPrefixes,
+                             UUID userUUID) {
+        this(nick, user, host, nickPrefixes, userUUID, false);
+    }
+
+    public MessageSenderInfo(String nick, String user, String host, NickPrefixList nickPrefixes,
+                             UUID userUUID, boolean automated) {
         super(nick, nickPrefixes);
         this.user = user;
         this.host = host;
         this.userUUID = userUUID;
+        this.automated = automated;
     }
 
     public String getUser() {
@@ -25,6 +33,10 @@ public class MessageSenderInfo extends NickWithPrefix {
 
     public UUID getUserUUID() {
         return userUUID;
+    }
+
+    public boolean isAutomated() {
+        return automated;
     }
 
 }
