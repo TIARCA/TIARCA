e9ad1edd6ecb643135dee5de8dfa83afbf05e75e
TIARCA <marcovange@duck.com>
2026-09-11 11:12:12 +0200

    Allow message sender automation metadata

--- chatlib/src/main/java/io/mrarm/chatlib/irc/MessagePrefix.java
+++ chatlib/src/main/java/io/mrarm/chatlib/irc/MessagePrefix.java
@@ -30,9 +30,15 @@ public class MessagePrefix {
     }
 
     public MessageSenderInfo toSenderInfo(UUID userUUID, ChannelData channelData) {
-        ChannelData.Member memberInfo = channelData != null && userUUID != null ? channelData.getMember(userUUID) : null;
+        return toSenderInfo(userUUID, channelData, false);
+    }
+
+    public MessageSenderInfo toSenderInfo(UUID userUUID, ChannelData channelData,
+                                          boolean automated) {
+        ChannelData.Member memberInfo = channelData != null && userUUID != null ?
+                channelData.getMember(userUUID) : null;
         return new MessageSenderInfo(getNick(), getUser(), getHost(),
-                memberInfo != null ? memberInfo.getNickPrefixes() : null, userUUID);
+                memberInfo != null ? memberInfo.getNickPrefixes() : null, userUUID, automated);
     }
 
     // sent by server
