e05991a320cb81db94501933de18dcaadd1b4b88
TIARCA <marcovange@duck.com>
2026-09-11 11:11:37 +0200

    Add monochrome bot appearance preference

---
+++ app/src/main/java/io/mrarm/irc/config/AutomatedSenderSettings.java
@@ -0,0 +1,10 @@
+package io.mrarm.irc.config;
+
+/** Visual-only preference for automated IRC identities. */
+public final class AutomatedSenderSettings {
+
+    public static final String PREF_MONOCHROME_BOTS = "monochrome_bots";
+
+    private AutomatedSenderSettings() {
+    }
+}
