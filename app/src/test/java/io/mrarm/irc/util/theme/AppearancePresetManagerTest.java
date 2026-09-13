a33bfa3a034251a1039ce359d70ee42392a7843a
TIARCA <marcovange@duck.com>
2026-09-13 21:24:30 +0200

    test: keep language outside portable preset options

--- app/src/test/java/io/mrarm/irc/util/theme/AppearancePresetManagerTest.java
+++ app/src/test/java/io/mrarm/irc/util/theme/AppearancePresetManagerTest.java
@@ -28,7 +28,6 @@ import io.mrarm.irc.config.MessageFormatSettings;
 import io.mrarm.irc.config.QuickCommandSettings;
 import io.mrarm.irc.config.RightClockSettings;
 import io.mrarm.irc.config.SettingsHelper;
-import io.mrarm.irc.util.AppLocaleManager;
 import io.mrarm.irc.util.DefaultPreferences;
 import io.mrarm.irc.util.MessageBuilder;
 
@@ -135,7 +134,6 @@ public class AppearancePresetManagerTest {
         manager.applyPreset(AppearancePreset.IRC_DARK);
 
         preferences.edit()
-                .putString(AppLocaleManager.PREF_APP_LANGUAGE, "it")
                 .putBoolean(ChatSettings.PREF_GLOBAL_FONT_ENABLED, true)
                 .putBoolean(ChatSettings.PREF_TEXT_AUTOCORRECT_ENABLED, false)
                 .putBoolean(ChatSettings.PREF_SEND_BOX_ALWAYS_MULTILINE, true)
