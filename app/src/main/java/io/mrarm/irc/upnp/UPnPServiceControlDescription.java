d5cab620f3390c14dc5a11b96205ab7576fcc10a
TIARCA <marcovange@duck.com>
2026-09-23 20:32:13 +0200

    Use local UPnP HTTP transport for XML descriptions

--- app/src/main/java/io/mrarm/irc/upnp/UPnPServiceControlDescription.java
+++ app/src/main/java/io/mrarm/irc/upnp/UPnPServiceControlDescription.java
@@ -21,7 +21,8 @@ public class UPnPServiceControlDescription {
     private List<Action> mActionList;
 
     public void loadFromUrl(String url) throws IOException, SAXException {
-        URLConnection connection = new URL(url).openConnection();
+        UPnPHttpClient.Response response = UPnPHttpClient.get(new URL(url));
+        response.requireSuccess();
         XMLReader reader;
         try {
             reader = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
@@ -29,7 +30,7 @@ public class UPnPServiceControlDescription {
             throw new RuntimeException(e);
         }
         reader.setContentHandler(new SAXRootParser(reader, this));
-        reader.parse(new InputSource(connection.getInputStream()));
+        reader.parse(new InputSource(new ByteArrayInputStream(response.getBody())));
     }
 
     public List<Action> getActionList() {
